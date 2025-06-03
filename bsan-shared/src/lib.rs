f9b1fd4682cc30b58322e578ff55d08f5316a5c9
Rafayel Amirkhanyan <rafayel.amirkhanyan@gmail.com>
2025-06-03 12:29:54 -0400

    task(#11): Semi-ported `diagnostics.rs` and some of `tree.rs` from Miri

--- bsan-shared/src/lib.rs
+++ bsan-shared/src/lib.rs
@@ -1,11 +1,16 @@
 #![cfg_attr(not(test), no_std)]
 #![allow(dead_code)]
-#![feature(allocator_api)]
 
+#[macro_use]
 extern crate alloc;
 
-pub mod foreign_access_skipping;
-pub mod helpers;
-pub mod perms;
-pub mod range_map;
+mod foreign_access_skipping;
+mod helpers;
+mod perms;
+mod range_map;
+mod types;
+pub use foreign_access_skipping::*;
+pub use helpers::*;
+pub use perms::*;
+pub use range_map::*;
 pub mod types;
