e3519d0e5814b261d89fb82fdaddfcdd9500efe3
Ian McCormack <icmccorm@cs.cmu.edu>
2025-06-09 14:01:21 +0000

    Added support for auto-updating config.

--- bsan-script/src/setup.rs
+++ bsan-script/src/setup.rs
@@ -1,24 +1,27 @@
-use std::fs::{self};
-use std::path::Path;
 use anyhow::Result;
 use path_macro::path;
 use rustc_version::VersionMeta;
+use std::fs::{self};
+use std::path::Path;
 use xshell::{cmd, Shell};
 
 use crate::env::BsanConfig;
 use crate::utils::{self, active_toolchain, show_error, version_meta};
 use crate::TOOLCHAIN_NAME;
 
-pub fn setup(sh: &Shell,
+pub fn setup(
+    sh: &Shell,
     host: &VersionMeta,
-    config: &BsanConfig,
-    toolchain_dir: &Path) -> Result<VersionMeta> {
+    config: &mut BsanConfig,
+    toolchain_dir: &Path,
+) -> Result<VersionMeta> {
     // If we have the `bsan` toolchain installed, then we've either already
     // run the setup script, or we're in our Docker container, which has all of
     // the dependencies that we need. Once we set the active toolchain, we can
-    // bail out. 
-    if let Ok(meta) = version_meta(sh, TOOLCHAIN_NAME) 
-        && Some(&config.sha) == host.commit_hash.as_ref() {
+    // bail out.
+    if let Ok(meta) = version_meta(sh, TOOLCHAIN_NAME)
+        && Some(&config.sha) == host.commit_hash.as_ref()
+    {
         if active_toolchain()? != TOOLCHAIN_NAME {
             cmd!(sh, "rustup override set {TOOLCHAIN_NAME}").run()?;
         }
@@ -40,20 +43,20 @@ pub fn setup(sh: &Shell,
 
     // If we've passed these checks, then let's do the expensive step of
     // downloading and installing our custom toolchain.
-    toolchain(sh, host, config, toolchain_dir)
-}
+    let meta = toolchain(sh, host, config, toolchain_dir)?;
 
+    Ok(meta)
+}
 
 fn toolchain(
     sh: &Shell,
     host: &VersionMeta,
-    config: &BsanConfig,
+    config: &mut BsanConfig,
     toolchain_dir: &Path,
 ) -> Result<VersionMeta> {
-
     let target = &host.host;
     let version = &config.version;
-    let archive_postfix: String = format!("{version}-{target}.tar.xz");
+    let archive_postfix: String = format!("{version}-dev-{target}.tar.xz");
     let artifact_url = path!(&config.artifact_url / &config.tag);
     let help_on_error = "Failed to download the custom Rust toolchain.";
 
@@ -87,7 +90,12 @@ fn toolchain(
 
     cmd!(sh, "rustup toolchain uninstall {TOOLCHAIN_NAME}").quiet().run()?;
     cmd!(sh, "rustup toolchain link {TOOLCHAIN_NAME} {toolchain_dir}").quiet().run()?;
-    cmd!(sh, "rustup override set {TOOLCHAIN_NAME}").quiet().run()?;
 
-    version_meta(sh, TOOLCHAIN_NAME)
+    let meta = version_meta(sh, TOOLCHAIN_NAME)?;
+    config.sha =
+        meta.commit_hash.clone().expect("Unable to resolve commit hash for latest toolchain.");
+    config.version = meta.semver.to_string();
+
+    cmd!(sh, "rustup override set {TOOLCHAIN_NAME}").quiet().run()?;
+    Ok(meta)
 }
